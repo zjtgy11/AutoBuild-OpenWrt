2473201a7b5ddb691dc73fff0929d23bf87a7d58
zjtgy11 <30390790+zjtgy11@users.noreply.github.com>
2022-09-15 16:48:57 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -11,8 +11,8 @@ sed -i 's/192.168.0.1/192.168.0.200/g' openwrt/package/base-files/files/bin/conf
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
-#3. Replace with JerryKuKu’s Argon
-#rm openwrt/package/lean/luci-theme-argon -rf
+3. Replace with JerryKuKu’s Argon
+rm openwrt/package/lean/luci-theme-argon -rf
 
 #4. 修改时区
 sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
